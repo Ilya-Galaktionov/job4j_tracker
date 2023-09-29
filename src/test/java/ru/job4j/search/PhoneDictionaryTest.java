61a79a4582622cc10ca695f21bb896834ff2cfc6
Ilya Galaktionov <igalaktionov1@gmail.com>
2023-09-29 18:01:44 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PhoneDictionaryTest {
+    @Test
+    void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    void whenFindIsEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons).isEmpty();
+    }
+}
