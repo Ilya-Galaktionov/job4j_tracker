0495bc590feb7897aadd3278f071ee38555b4eb8
Ilya Galaktionov <igalaktionov1@gmail.com>
2023-10-04 22:52:09 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PassportOfficeTest {
+    @Test
+    void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    void whenCheckDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen anotherCitizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(anotherCitizen)).isFalse();
+    }
+}
