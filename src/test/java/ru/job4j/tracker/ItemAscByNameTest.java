3aa38e80838d967747c87575b01d4fb2c1c984a4
Ilya Galaktionov <igalaktionov1@gmail.com>
2023-10-09 15:29:41 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ItemAscByNameTest {
+    @Test
+    void whenItemAscByNameOder() {
+        Item one = new Item("1");
+        Item two = new Item("2");
+        Item three = new Item("3");
+        List<Item> items = Arrays.asList(three, one, two);
+        Collections.sort(items, new ItemAscByName());
+        List<Item> expected = Arrays.asList(one, two, three);
+        assertThat(items).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ItemDescByNameTest {
+    @Test
+    void whenItemDescByNameOder() {
+        Item one = new Item("1");
+        Item two = new Item("2");
+        Item three = new Item("3");
+        List<Item> items = Arrays.asList(one, three, two);
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = Arrays.asList(three, two, one);
+        assertThat(items).isEqualTo(expected);
+    }
+}
